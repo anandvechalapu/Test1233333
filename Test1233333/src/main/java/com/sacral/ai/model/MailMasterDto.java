838a7dd4eb7229493b510e20e19f2e9f55770fb5
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-20 10:47:19 +0530

    initial commit

---
+++ Test1233333/src/main/java/com/sacral/ai/model/MailMasterDto.java
@@ -0,0 +1,44 @@
+package com.sacral.ai.model;
+
+public class MailMasterDto {
+
+    private String sender;
+    private String recipient;
+    private String subject;
+    private String message;
+
+    public MailMasterDto() {
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
