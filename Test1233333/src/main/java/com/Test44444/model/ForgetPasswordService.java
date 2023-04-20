e8c68d8dd94785a419e0aec9bbfc5751acd60478
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-20 10:47:31 +0530

    initial commit

---
+++ Test1233333/src/main/java/com/Test44444/model/ForgetPasswordService.java
@@ -0,0 +1,59 @@
+package com.Test44444.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "forget_password_service")
+public class ForgetPasswordService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String otp;
+
+    private String password;
+
+    public ForgetPasswordService() {
+    
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
